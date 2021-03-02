1226323f05ed037657e5e5e56b073deb8fb2c06e
breeze <nanhuaijin77@gmail.com>
2021-03-02 16:25:35 +0800

    feat：项目初始化、添加README文件

---
+++ src/main/java/com/code/JavaInterviewAPP.java
@@ -0,0 +1,16 @@
+package com.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : breeze
+ * @date : 2020/12/9
+ * @description : 主启动
+ */
+@SpringBootApplication
+public class JavaInterviewAPP {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInterviewAPP.class, args);
+    }
+}
